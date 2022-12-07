a2c23a805bd4cdc4be824678ba3f158fa7822607
Erick Milan <erickmilan1002@gmail.com>
2022-12-07 12:51:06 -0500

    delete comments

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-
-
 const Router = () => {
-
   return (
     <BrowserRouter>
       <Routes>
